d9c13479f1f20cb25c6118d5ea237461fa8d406a
vasan-r <aavasan2002@gmail.com>
2024-12-25 12:20:24 +0530

    play with route, get,post,delete and post

--- src/app.js
+++ src/app.js
@@ -2,20 +2,32 @@ const express = require("express");
 
 const app = express();
 
+app.get("/test", (req, res) => {
+  res.send({ fistname: "vasan", lastName: "r" });
+});
+
+app.post("/test", (req, res) => {
+  res.send("database was sucessfully created");
+});
+
+app.delete("/test", (req, res) => {
+  res.send("database deleted sucessfully");
+});
+
+app.patch("/test", (req, res) => {
+  res.send("database updated sucessfully");
+});
+
 app.use("/test", (req, res) => {
   res.send("hello from test");
 });
 
+app.use("/", function (req, res) {
+  res.send("hello from empty ");
+});
+
 app.use("/hello", function (req, res) {
   res.send("hello from hello");
-});
-
-app.use("/mango", function (req, res) {
-  res.send("hello from mango");
-});
-
-app.use(function (req, res) {
-  res.send("hello from server");
 });
 
 app.listen(3000, () => {
