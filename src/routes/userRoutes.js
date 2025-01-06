aa8fa1b38bad76895c599540bdf168cdcf153044
vasan-r <aavasan2002@gmail.com>
2025-01-06 12:47:14 +0530

    feed & pagination in userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const { find } = require("../models/user");
+const User = require("../models/user");
 const userRoutes = express.Router();
 
 const UserSafeDate = "firstName lastName about photoUrl";
@@ -46,6 +47,43 @@ userRoutes.get("/user/connections", userAuth, async (req, res) => {
     res.json(data);
   } catch (err) {
     res.status(404).send("Error: " + err.message);
+  }
+});
+
+userRoutes.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (page - 1) * limit;
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUserFromFeed = new Set();
+
+    connectionRequest.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId.toString());
+      hideUserFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(UserSafeDate)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ data: users });
+  } catch (err) {
+    res.status(500).send("Error: " + err.message);
   }
 });
 
